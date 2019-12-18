747ea5d8674fbb025be10f7193680d0104f3f705
Sivanesh <sivaneshraina@gmail.com>
2019-12-18 09:40:46 +0530

    updated treeview

--- tree-view.js
+++ tree-view.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 module.exports = function(location) {
   const files = readCurrentDirectory(location);
   allFiles = iterateFilesAndFolders(files, location);
-  console.log('allFiles', allFiles[4]);
+  console.log('allFiles', allFiles);
 };
 
 function readCurrentDirectory(location, withFileTypes = true) {
@@ -12,8 +12,32 @@ function readCurrentDirectory(location, withFileTypes = true) {
 
 function writeLine() {}
 
-// Array structure
 function iterateFilesAndFolders(files, location) {
+  const allFiles = {};
+
+  files.forEach(file => {
+    const currentLocation = `${location.replace(__dirname, '')}\\${file.name}`;
+
+    if (file.isDirectory()) {
+      allFiles[currentLocation] = file;
+      const subDirectoryLocation = `${location}\\${file.name}`;
+      const subDirectoryFiles = readCurrentDirectory(subDirectoryLocation);
+      allFiles[currentLocation].children = iterateFilesAndFolders(
+        subDirectoryFiles,
+        subDirectoryLocation
+      );
+    } else if (file.isFile()) {
+      allFiles[currentLocation] = file.name;
+    } else {
+      console.warn('[treeView] Its not a file as well as directory.', file);
+    }
+  });
+
+  return allFiles;
+}
+
+// Array structure
+function ArrayIterateFilesAndFolders(files, location) {
   const allFiles = [];
   files.forEach(file => {
     if (file.isDirectory()) {
